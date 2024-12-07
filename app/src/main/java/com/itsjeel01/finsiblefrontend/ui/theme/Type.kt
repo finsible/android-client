06b13b362e9c2b45477242d067fa65ef335b7717
Jeel Patel <itsjeel01@gmail.com>
2024-12-07 15:49:04 +0530

    Change the Font to Rubik

--- app/src/main/java/com/itsjeel01/finsiblefrontend/ui/theme/Type.kt
+++ app/src/main/java/com/itsjeel01/finsiblefrontend/ui/theme/Type.kt
@@ -14,14 +14,14 @@ val provider = GoogleFont.Provider(
 
 val bodyFontFamily = FontFamily(
     Font(
-        googleFont = GoogleFont("Inter Tight"),
+        googleFont = GoogleFont("Rubik"),
         fontProvider = provider,
     )
 )
 
 val displayFontFamily = FontFamily(
     Font(
-        googleFont = GoogleFont("Manrope"),
+        googleFont = GoogleFont("Rubik"),
         fontProvider = provider,
     )
 )

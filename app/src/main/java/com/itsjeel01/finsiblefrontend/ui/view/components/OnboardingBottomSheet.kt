03c9d42d4d55c9644a0ab75934c76e3c4bbb004b
Jeel Patel <itsjeel01@gmail.com>
2024-12-07 15:57:40 +0530

    Add UI Enhancements

--- app/src/main/java/com/itsjeel01/finsiblefrontend/ui/view/components/OnboardingBottomSheet.kt
+++ app/src/main/java/com/itsjeel01/finsiblefrontend/ui/view/components/OnboardingBottomSheet.kt
@@ -1,5 +1,15 @@
 package com.itsjeel01.finsiblefrontend.ui.view.components
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.SizeTransform
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -11,19 +21,23 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.itsjeel01.finsiblefrontend.R
 import com.itsjeel01.finsiblefrontend.data.slides
 import com.itsjeel01.finsiblefrontend.ui.viewmodel.OnboardingViewModel
 import com.itsjeel01.finsiblefrontend.utils.signInWithGoogle
@@ -39,7 +53,7 @@ fun OnboardingBottomSheet(viewModel: OnboardingViewModel, modifier: Modifier) {
     val context = LocalContext.current
     val coroutineScope = rememberCoroutineScope()
 
-    fun nextSlide() {
+    fun navigateToNext() {
         if (currentSlide == slides.lastIndex) {
             signInWithGoogle(context, coroutineScope)
             return
@@ -49,30 +63,61 @@ fun OnboardingBottomSheet(viewModel: OnboardingViewModel, modifier: Modifier) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
-            .fillMaxWidth()
-            .background(
-                color = MaterialTheme.colorScheme.secondaryContainer,
-                shape = RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp)
+            .shadow(
+                elevation = 24.dp,
+                ambientColor = MaterialTheme.colorScheme.primary,
+                spotColor = MaterialTheme.colorScheme.primary,
+                shape = RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp),
             )
-            .padding(horizontal = 16.dp, vertical = 24.dp)
+            .background(
+                shape = RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp),
+                color = MaterialTheme.colorScheme.background,
+            )
+            .fillMaxWidth()
+            .padding(start = 24.dp, end = 24.dp, top = 24.dp, bottom = 48.dp)
     ) {
 
-        // Headline
-        Text(
-            text = slides[currentSlide].headline,
-            style = MaterialTheme.typography.headlineLarge,
-            modifier = Modifier.padding(vertical = 8.dp)
-        )
+        AnimatedContent(
+            targetState = slides[currentSlide],
+            transitionSpec = {
+                (slideInHorizontally(
+                    initialOffsetX = { it },
+                    animationSpec = tween(durationMillis = 1200)
+                ) + fadeIn(animationSpec = tween(durationMillis = 300)))
+                    .togetherWith(
+                        slideOutVertically(
+                            targetOffsetY = { -it },
+                            animationSpec = tween(durationMillis = 1200)
+                        ) + fadeOut(animationSpec = tween(durationMillis = 300))
+                    ).using(SizeTransform(clip = true))
+            },
+            label = "AnimatedContent"
+        ) { slide ->
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                // Headline
+                Text(
+                    text = slide.headline,
+                    style = MaterialTheme.typography.displaySmall.copy(
+                        fontWeight = FontWeight.SemiBold,
+                        color = MaterialTheme.colorScheme.primary
+                    ),
+                    modifier = Modifier.padding(vertical = 8.dp)
+                )
 
-        // Description
-        Text(
-            text = slides[currentSlide].description,
-            style = MaterialTheme.typography.bodyMedium,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
+                // Description
+                Text(
+                    text = slide.description,
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.bodyMedium.copy(
+                        fontWeight = FontWeight.Normal,
+                        color = MaterialTheme.colorScheme.onSecondaryContainer
+                    ),
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+            }
+        }
 
-        Spacer(modifier = Modifier.height(8.dp))
+        Spacer(modifier = Modifier.height(16.dp))
 
         // Slider Indicators
         Row(
@@ -81,15 +126,19 @@ fun OnboardingBottomSheet(viewModel: OnboardingViewModel, modifier: Modifier) {
             modifier = Modifier.fillMaxWidth()
         ) {
             slides.indices.forEach { index ->
+                val size by animateDpAsState(
+                    targetValue = if (index == currentSlide) 16.dp else 8.dp,
+                    label = "AnimateDpAsState"
+                )
+                val color by animateColorAsState(
+                    targetValue = if (index == currentSlide) MaterialTheme.colorScheme.primary else Color.Gray,
+                    label = "AnimateColorAsState"
+                )
                 Box(
                     modifier = Modifier
                         .padding(horizontal = 2.dp)
-                        .size(if (index == currentSlide) (16.dp) else 8.dp)
-                        .background(
-                            if (index == currentSlide) MaterialTheme.colorScheme.primary
-                            else Color.Gray,
-                            shape = RoundedCornerShape(50)
-                        )
+                        .size(if (index == currentSlide) 16.dp else 8.dp)
+                        .background(color, shape = RoundedCornerShape(50))
                         .clickable { viewModel.updateSlide(index) }
                 )
             }
@@ -98,12 +147,18 @@ fun OnboardingBottomSheet(viewModel: OnboardingViewModel, modifier: Modifier) {
         Spacer(modifier = Modifier.height(32.dp))
 
         // Dynamic Button for navigation
-        Button(
-            onClick = { nextSlide() },
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text(text = buttonLabel)
-        }
+        FinsibleButton(
+            label = buttonLabel,
+            onClick = { navigateToNext() },
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    color = MaterialTheme.colorScheme.primary,
+                    shape = RoundedCornerShape(corner = CornerSize(4.dp))
+                ),
+            cornerSize = 8.dp,
+            labelColor = MaterialTheme.colorScheme.onPrimary,
+            iconDrawable = R.drawable.arrow_right_icon
+        )
     }
 }
-
--- app/src/main/java/com/itsjeel01/finsiblefrontend/ui/view/screens/OnboardingScreen.kt
+++ app/src/main/java/com/itsjeel01/finsiblefrontend/ui/view/screens/OnboardingScreen.kt
@@ -1,19 +1,30 @@
 package com.itsjeel01.finsiblefrontend.ui.view.screens
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.SizeTransform
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,31 +38,64 @@ import com.itsjeel01.finsiblefrontend.ui.viewmodel.OnboardingViewModel
 fun OnboardingScreen() {
     val onboardingViewModel: OnboardingViewModel = hiltViewModel()
 
+    val topAppBarColors = TopAppBarColors(
+        containerColor = MaterialTheme.colorScheme.background,
+        titleContentColor = MaterialTheme.colorScheme.background,
+        actionIconContentColor = MaterialTheme.colorScheme.background,
+        scrolledContainerColor = MaterialTheme.colorScheme.background,
+        navigationIconContentColor = MaterialTheme.colorScheme.background,
+    )
+
     Scaffold(
         modifier = Modifier.fillMaxSize(),
-        topBar = { TopAppBar(title = { Text(text = "") }) }
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "") },
+                modifier = Modifier.background(MaterialTheme.colorScheme.background),
+                colors = topAppBarColors
+            )
+        }
     ) { padding ->
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(padding)
+                .padding(top = padding.calculateTopPadding())
+                .background(MaterialTheme.colorScheme.background),
+            contentAlignment = Alignment.Center
         ) {
             // Dynamic illustration at the top
-            Image(
-                painter = painterResource(id = slides[onboardingViewModel.currentSlide.collectAsState().value].illustration),
-                contentDescription = "Onboarding Illustration",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight()
-                    .align(Alignment.TopCenter)
-                    .padding(bottom = 100.dp),
-                alignment = Alignment.Center
-            )
+            AnimatedContent(
+                targetState = slides[onboardingViewModel.currentSlide.collectAsState().value],
+                transitionSpec = {
+                    (slideInVertically(
+                        initialOffsetY = { it },
+                        animationSpec = tween(durationMillis = 1200)
+                    ) + fadeIn(animationSpec = tween(durationMillis = 300)))
+                        .togetherWith(
+                            slideOutHorizontally(
+                                targetOffsetX = { -it },
+                                animationSpec = tween(durationMillis = 1200)
+                            ) + fadeOut(animationSpec = tween(durationMillis = 300))
+                        ).using(SizeTransform(clip = true))
+                },
+                label = "AnimatedContent"
+            ) { slide ->
+                Image(
+                    painter = painterResource(id = slide.illustration),
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .fillMaxWidth()
+                        .padding(bottom = 250.dp, start = 16.dp, end = 16.dp),
+                    contentDescription = "Onboarding Illustration",
+                    contentScale = ContentScale.FillWidth,
+                    alignment = Alignment.Center
+                )
+            }
 
             // Sticky Bottom Sheet
             OnboardingBottomSheet(
                 viewModel = onboardingViewModel,
-                modifier = Modifier.align(Alignment.BottomCenter)
+                modifier = Modifier.align(Alignment.BottomCenter),
             )
         }
     }

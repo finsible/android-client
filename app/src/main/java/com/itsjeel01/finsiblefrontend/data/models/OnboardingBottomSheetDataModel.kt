39ff4f34e50eb4bdc5c3e7c7f2706348dd1f38d3
Jeel Patel <itsjeel01@gmail.com>
2025-01-26 19:02:22 +0530

    Remove OnboardingBottomSheetDataModel.kt

--- app/src/main/java/com/itsjeel01/finsiblefrontend/data/models/OnboardingBottomSheetDataModel.kt
+++
@@ -1,7 +0,0 @@
-package com.itsjeel01.finsiblefrontend.data.models
-
-data class OnboardingBottomSheetDataModel(
-    val illustration: Int,
-    val headline: String,
-    val description: String,
-)

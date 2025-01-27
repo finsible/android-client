14f6d7196ce683c7d6387e01816e3a7589c95e7f
Jeel Patel <itsjeel01@gmail.com>
2025-01-27 22:36:03 +0530

    Remove redundant code

--- app/src/main/java/com/itsjeel01/finsiblefrontend/MyApplication.kt
+++ app/src/main/java/com/itsjeel01/finsiblefrontend/MyApplication.kt
@@ -1,21 +1,13 @@
-package com.itsjeel01.finsiblefrontend;
+package com.itsjeel01.finsiblefrontend
 
 import android.app.Application
 import com.itsjeel01.finsiblefrontend.module.ObjectBoxModule
-import com.itsjeel01.finsiblefrontend.utils.PreferenceManager
 import dagger.hilt.android.HiltAndroidApp
-import io.objectbox.BoxStore
 
 @HiltAndroidApp
 class MyApplication : Application() {
-    private lateinit var boxStore: BoxStore
-
     override fun onCreate() {
         super.onCreate()
         ObjectBoxModule.init(this)
-    }
-
-    fun tokenProvider(): String? {
-        return PreferenceManager(this).getJwt()
     }
 }

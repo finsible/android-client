6dc40af61bb048739d3e2dfe059f17aaae72f281
Jeel Patel <itsjeel01@gmail.com>
2025-04-01 15:43:15 +0530

    Refactor New Transaction Form screen
    -   Removes the ViewModel from the `NewTransactionForm` composable
    -   Removes the previews from the `NewTransactionForm`
    -   Moves the segmented control outside the inner `Column`
    -   Moves the `NewTransactionAmountTextField` and `NewTransactionDatePicker` into a horizontal row.
    -   Adds outer and inner paddings for a more polished look.
    -   Add `screenWidth` property to `TransactionSegmentedControl`

--- app/src/main/java/com/itsjeel01/finsiblefrontend/ui/view/screens/NewTransactionForm.kt
+++ app/src/main/java/com/itsjeel01/finsiblefrontend/ui/view/screens/NewTransactionForm.kt
@@ -1,66 +1,54 @@
 package com.itsjeel01.finsiblefrontend.ui.view.screens
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
-import com.itsjeel01.finsiblefrontend.data.TransactionType
-import com.itsjeel01.finsiblefrontend.ui.theme.getTransactionColor
 import com.itsjeel01.finsiblefrontend.ui.view.components.NewTransactionAmountTextField
 import com.itsjeel01.finsiblefrontend.ui.view.components.NewTransactionDatePicker
-import com.itsjeel01.finsiblefrontend.ui.view.components.TransactionCategoryDropdown
 import com.itsjeel01.finsiblefrontend.ui.view.components.TransactionSegmentedControl
-import com.itsjeel01.finsiblefrontend.ui.viewmodel.NewTransactionFormViewModel
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun NewTransactionForm() {
-    val newTransactionFormViewModel: NewTransactionFormViewModel = hiltViewModel()
-    val transactionType = newTransactionFormViewModel.transactionTypeState.collectAsState().value
-    val transactionAmount =
-        newTransactionFormViewModel.transactionAmountState.collectAsState().value
     val screenHeight = LocalConfiguration.current.screenHeightDp
     val screenWidth = LocalConfiguration.current.screenWidthDp
 
-    Scaffold { padding ->
+    val outerPadding = (0.02 * screenHeight).dp
+    val horizontalInnerPadding = (0.025 * screenWidth).dp
+    val verticalInnerTopPadding = (0.001 * screenHeight).dp
+    val inputFieldSpacing = (screenWidth * 0.05).dp
+
+    Scaffold { _ ->
         Column(
             modifier = Modifier
                 .windowInsetsPadding(WindowInsets.systemBars)
-                .padding(horizontal = (0.035 * screenWidth).dp, vertical = (0.02 * screenHeight).dp)
+                .padding(all = outerPadding)
         ) {
-            TransactionSegmentedControl()
+            // Transaction type selector
+            TransactionSegmentedControl(screenWidth = screenWidth)
+
+            // Form fields container
             Column(
-                modifier = Modifier.padding(horizontal = (0.03 * screenWidth).dp)
-            ) {
-                NewTransactionAmountTextField(
-                    initialAmount = transactionAmount,
-                    onAmountChange = { amount ->
-                        newTransactionFormViewModel.setTransactionAmount(amount)
-                    },
-                    accentColor = getTransactionColor(type = transactionType)
+                modifier = Modifier.padding(
+                    horizontal = horizontalInnerPadding,
+                    vertical = verticalInnerTopPadding
                 )
-                NewTransactionDatePicker()
-                Row(modifier = Modifier.fillMaxWidth()) {
-                    if (transactionType != TransactionType.TRANSFER)
-                        TransactionCategoryDropdown(modifier = Modifier.weight(1F))
+            ) {
+                Row(horizontalArrangement = Arrangement.spacedBy(inputFieldSpacing)) {
+                    NewTransactionAmountTextField(modifier = Modifier.weight(1f))
+                    NewTransactionDatePicker(modifier = Modifier.weight(1f))
                 }
             }
         }
     }
-}
-
-@Composable
-@Preview
-fun NewTransactionFormPreview() {
-    NewTransactionForm()
 }

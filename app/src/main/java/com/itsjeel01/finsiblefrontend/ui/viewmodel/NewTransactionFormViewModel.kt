ba1777784b9e66cb2690ad7ca6acaeacd8cb2bb2
Jeel Patel <itsjeel01@gmail.com>
2025-04-01 14:54:23 +0530

    Refactor NewTransactionFormViewModel for improved readability and structure

--- app/src/main/java/com/itsjeel01/finsiblefrontend/ui/viewmodel/NewTransactionFormViewModel.kt
+++ app/src/main/java/com/itsjeel01/finsiblefrontend/ui/viewmodel/NewTransactionFormViewModel.kt
@@ -10,25 +10,29 @@ import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class NewTransactionFormViewModel @Inject constructor(private val categoryLocalRepository: CategoryLocalRepository) :
-    ViewModel() {
+class NewTransactionFormViewModel @Inject constructor(
+    private val categoryLocalRepository: CategoryLocalRepository,
+) : ViewModel() {
+    // Transaction type
     private val _transactionTypeState = MutableStateFlow(TransactionType.EXPENSE)
     val transactionTypeState: StateFlow<TransactionType> = _transactionTypeState
 
+    // Transaction date
     private val _transactionDateState = MutableStateFlow(System.currentTimeMillis())
     val transactionDateState: StateFlow<Long> = _transactionDateState
 
+    // Transaction amount
     private val _transactionAmountState = MutableStateFlow<Double?>(null)
     val transactionAmountState: StateFlow<Double?> = _transactionAmountState
 
     private val _currentCategoriesState =
-        MutableStateFlow(categoryLocalRepository.getAllCategories()
-            .filter { it.type == _transactionTypeState.value })
+        MutableStateFlow(
+            categoryLocalRepository.getAllCategories()
+                .filter { it.type == _transactionTypeState.value })
     val currentCategoriesState: StateFlow<List<CategoryEntity>> = _currentCategoriesState
 
-    private val _transactionCategoryState =
-        MutableStateFlow(currentCategoriesState.value.first())
-    val transactionCategoryState: MutableStateFlow<CategoryEntity> = _transactionCategoryState
+    // Selected category
+    private val _transactionCategoryState = MutableStateFlow<CategoryEntity>(currentCategoriesState.value.first())
 
     init {
         loadCategories()
@@ -42,7 +46,9 @@ class NewTransactionFormViewModel @Inject constructor(private val categoryLocalR
 
     fun setTransactionType(transactionType: TransactionType) {
         _transactionTypeState.value = transactionType
-        if (transactionType != TransactionType.TRANSFER) loadCategories()
+        if (transactionType != TransactionType.TRANSFER) {
+            loadCategories()
+        }
     }
 
     fun setTransactionDate(transactionDate: Long) {

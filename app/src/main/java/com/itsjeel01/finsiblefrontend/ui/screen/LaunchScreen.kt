1488e20450de9f5d9efe6a4cd5c81455c323e8aa
Jeel Patel <itsjeel01@gmail.com>
2025-09-04 22:00:39 +0530

    Refactor LaunchScreen and navigation routes
    - LaunchScreen now uses `LaunchedEffect` for navigation based on `authState` and shows a loading indicator.
    - `Routes` sealed class is renamed to `AppRoutes` and simplified.
    - `launchNavGraph` is renamed to `appNavGraph` and updated to use new route definitions and navigation logic.

--- app/src/main/java/com/itsjeel01/finsiblefrontend/ui/screen/LaunchScreen.kt
+++ app/src/main/java/com/itsjeel01/finsiblefrontend/ui/screen/LaunchScreen.kt
@@ -1,21 +1,45 @@
 package com.itsjeel01.finsiblefrontend.ui.screen
 
+import android.util.Log
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavHostController
-import com.itsjeel01.finsiblefrontend.ui.data.AuthState
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.itsjeel01.finsiblefrontend.data.di.hiltLoadingManager
+import com.itsjeel01.finsiblefrontend.ui.model.AuthState
 import com.itsjeel01.finsiblefrontend.ui.viewmodel.AuthViewModel
 
-@Composable
-fun LaunchScreen(navHostController: NavHostController) {
-    val authViewModel: AuthViewModel = hiltViewModel()
-    val authState by authViewModel.authState.collectAsState()
+private val Tag = "LaunchScreen"
 
-    when (authState) {
-        is AuthState.Negative -> OnboardingScreen(navHostController)
-        AuthState.Positive -> DashboardScreen(navHostController)
-        AuthState.Loading -> OnboardingScreen(navHostController)
+@Composable
+fun LaunchScreen(
+    navigateToOnboarding: () -> Unit,
+    navigateToDashboard: () -> Unit,
+) {
+
+    val authViewModel: AuthViewModel = hiltViewModel()
+    val authState by authViewModel.authState.collectAsStateWithLifecycle()
+
+    LaunchedEffect(authState) {
+        when (authState) {
+            is AuthState.Negative -> {
+                Log.d(Tag, "AuthState = Negative; Navigating to Onboarding")
+                navigateToOnboarding()
+            }
+
+            AuthState.Positive -> {
+                Log.d(Tag, "AuthState = Positive; Navigating to Dashboard")
+                navigateToDashboard()
+            }
+
+            else -> {
+                Log.d(Tag, "AuthState = Loading")
+            }
+        }
+    }
+
+    if (authState is AuthState.Loading) {
+        hiltLoadingManager().show()
     }
 }

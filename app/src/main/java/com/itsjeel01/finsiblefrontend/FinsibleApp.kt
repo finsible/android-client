041619731cfc8106ccbe64ca2c3c58cf9e28a2eb
Jeel Patel <itsjeel01@gmail.com>
2026-01-10 00:05:01 +0530

    Integrate integrity resolver service on app launch

--- app/src/main/java/com/itsjeel01/finsiblefrontend/FinsibleApp.kt
+++ app/src/main/java/com/itsjeel01/finsiblefrontend/FinsibleApp.kt
@@ -4,8 +4,7 @@ import android.app.Application
 import com.itsjeel01.finsiblefrontend.common.logging.DebugLogTree
 import com.itsjeel01.finsiblefrontend.common.logging.Logger
 import com.itsjeel01.finsiblefrontend.common.logging.ReleaseLogTree
-import com.itsjeel01.finsiblefrontend.data.di.ObjectBoxModule
-import com.itsjeel01.finsiblefrontend.data.sync.NetworkMonitor
+import com.itsjeel01.finsiblefrontend.data.sync.IntegrityResolverService
 import com.itsjeel01.finsiblefrontend.data.sync.ScopeManager
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
@@ -17,20 +16,16 @@ class FinsibleApp : Application() {
     lateinit var scopeManager: ScopeManager
 
     @Inject
-    lateinit var networkMonitor: NetworkMonitor
+    lateinit var integrityResolverService: IntegrityResolverService
 
     override fun onCreate() {
         super.onCreate()
         initializeLogging()
-        ObjectBoxModule.init(this)
+        integrityResolverService.checkAndResolveOnLaunch()
     }
 
     override fun onTerminate() {
         super.onTerminate()
-        // Note: onTerminate() is only called in emulated environments, not on actual devices.
-        // This cleanup is primarily for testing. Production apps should rely on the logout
-        // cleanup path in AuthRepository for proper resource management.
-        networkMonitor.cleanup()
         scopeManager.shutdown()
     }
 

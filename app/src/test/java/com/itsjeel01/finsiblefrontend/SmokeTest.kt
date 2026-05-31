d36eff433b5ff3a7ae36ad0b60c779133aed5dda
Jeel Patel <itsjeel01@gmail.com>
2026-05-31 16:29:44 +0530

    Add MainDispatcherRule and SmokeTest for testing infrastructure

---
+++ app/src/test/java/com/itsjeel01/finsiblefrontend/SmokeTest.kt
@@ -0,0 +1,11 @@
+package com.itsjeel01.finsiblefrontend
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class SmokeTest {
+    @Test
+    fun `infrastructure is working`() {
+        assertThat(true).isTrue()
+    }
+}
